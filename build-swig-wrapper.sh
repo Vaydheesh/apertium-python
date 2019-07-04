9347884b01d0395119ffcdf32f4b266a46892977
vaydheesh <lokendras1998@gmail.com>
2019-07-04 19:39:17 +0530

    apertium-core wrapped 4/5

--- build-swig-wrapper.sh
+++ build-swig-wrapper.sh
@@ -4,12 +4,18 @@ set -e
 sudo apt-get install -y swig build-essential python3-setuptools
 git clone https://github.com/apertium/lttoolbox.git
 cd lttoolbox
-./autogen.sh --enable-python-bindings && make
+./autogen.sh --enable-python-bindings && make -j 4
 cd python
 python3 setup.py install
 
-git clone -b swig_wrapper https://github.com/Vaydheesh/apertium-lex-tools.git
+git clone https://github.com/apertium/apertium-lex-tools.git
 cd apertium-lex-tools
-./autogen.sh --enable-python-bindings && make
+./autogen.sh --enable-python-bindings && make -j 4
+cd python
+python3 setup.py install
+
+git clone -b swig_wrapper https://github.com/Vaydheesh/apertium.git
+cd apertium
+./autogen.sh --enable-python-bindings && make -j 30
 cd python
 python3 setup.py install
