e7575a56ba167789275a99d4fb40797f97121511
vaydheesh <lokendras1998@gmail.com>
2019-08-12 00:30:22 +0530

    skip: wrapper installation test on windows

--- tests/__init__.py
+++ tests/__init__.py
@@ -91,6 +91,7 @@ class TestInstallation(unittest.TestCase):
         importlib.reload(apertium)
         self.assertIn(language, apertium.analyzers, 'apetium.install_module not working')
 
+    @unittest.skipIf(platform.system() == 'Windows', 'wrappers not available for windows')
     def test_install_wrapper(self):
         apertium.installer.install_wrapper('python3-lttoolbox')
         if platform.system() == 'Linux':
